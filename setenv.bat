75869fde37b5cd676f2cc4c7ec9104d6e0b95693
Harri Kaimio <harri@kaimio.fi>
2004-02-22 21:15:10 +0000

    Moved to new machine, upgraded OJB & others. Not yet working Added also libraries to change control.

--- setenv.bat
+++ setenv.bat
@@ -1,7 +1,8 @@
 @echo off
 set LOG4J=c:\java\lib\log4j-1.2.7.jar
-set OJB=c:\java\lib\jakarta-ojb-0.9.9.jar
+set OJB=c:\java\lib\db-ojb-1.0.rc5.jar
 set CONFDIR=c:\java\photovault\conf
-set OJBDEPS=c:\java\jakarta-ojb\lib\commons-beanutils.jar;c:\java\jakarta-ojb\lib\commons-collections-2.0.jar;c:\java\jakarta-ojb\lib\commons-dbcp.jar;c:\java\jakarta-ojb\lib\commons-lang-1.0-mod.jar;c:\java\jakarta-ojb\lib\commons-logging.jar;c:\java\jakarta-ojb\lib\commons-pool.jar;c:\java\jakarta-ojb\lib\hsqldb.jar;c:\java\jakarta-ojb\lib\jboss-common.jar;c:\java\jakarta-ojb\lib\jboss-system.jar;c:\java\jakarta-ojb\lib\jca1.0.jar;c:\java\jakarta-ojb\lib\jcs.jar;c:\java\jakarta-ojb\lib\jdbc2_0-stdext.jar;c:\java\jakarta-ojb\lib\jmxri.jar;c:\java\jakarta-ojb\lib\jndi.jar;c:\java\jakarta-ojb\lib\jta-spec1_0_1.jar;c:\java\jakarta-ojb\lib\p6spy.jar;c:\java\jakarta-ojb\lib\proxy.jar;c:\java\jakarta-ojb\lib\xdoclet.jar;c:\java\jakarta-ojb\lib\xercesImpl.jar;c:\java\jakarta-ojb\lib\xml-apis.jar;c:\java\jakarta-ojb\lib\antlr.jar
+
+set OJBDEPS=lib\antlr.jar;lib\btree.jar;lib\commons-beanutils.jar;lib\commons-collections.jar;lib\commons-dbcp-1.1.jar;lib\commons-lang-2.0.jar;lib\commons-logging.jar;lib\commons-pool-1.1.jar;lib\db-ojb-1.0.rc5.jar;lib\hsqldb.jar;lib\j2ee.jar;lib\jakarta-regexp-1.3.jar;lib\jcs.jar;lib\jdo.jar;lib\jdori-enhancer.jar;lib\jdori.jar;lib\log4j-1.2.8.jar;lib\p6spy.jar;lib\prevayler.jar;lib\xdoclet-1.2b3-dev.jar;lib\xdoclet-ojb-module-1.2b3-dev.jar;lib\xercesImpl.jar;lib\xjavadoc-1.0.jar;lib\xml-apis.jar
 
 set CLASSPATH=%CONFDIR%;c:\java\junit3.8.1\junit.jar;c:\java\abbot-0.8.2\lib\abbot.jar;c:\java\lib\mysql-connector-java-3.0.2-beta-bin.jar;c:\java\lib\metadataExtractor.jar;c:\java\photovault\build\;%LOG4J%;%OJB%;%OJBDEPS%
