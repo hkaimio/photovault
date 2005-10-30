e90e5fcebcc1c87f0c187142e86afdeaccf7c8e5
Harri Kaimio <harri@kaimio.fi>
2005-10-30 19:41:17 +0000

    FIxed unit tests. SVN broken - this is a partial commit of the changes!!!!

--- db/create_test_data.sql
+++ db/create_test_data.sql
@@ -2,17 +2,20 @@
 -- Create folders for PhtoFolder test
 ---
  
-
-insert into photo_collections values ( 100, 0, "subfolderTest", "", NULL, null);
+delete from photo_collections;
+insert into photo_collections values ( 1, null, "Top", "", NULL, null);
+insert into photo_collections values ( 100, 1, "subfolderTest", "", NULL, null);
 insert into photo_collections values ( 101, 100, "Subfolder1", "", NULL, null);
 insert into photo_collections values ( 102, 100, "Subfolder2", "", NULL, null);
 insert into photo_collections values ( 103, 100, "Subfolder3", "", NULL, null);
 insert into photo_collections values ( 104, 100, "Subfolder4", "", NULL, null);
+insert into photo_collections values ( 105, 1, "testPhotoRetrieval", "", NULL, null);
 
+delete from photos;
+insert into photos(photo_id, description) values( 1, "testPhoto1" );
+insert into photos(photo_id, description) values( 2, "testPhotoRetrieval1" );
+insert into photos(photo_id, description) values( 3, "testPhotoRetrieval2" );
 
-insert into photo_collections values ( 105, 0, "testPhotoRetrieval", "", NULL, null);
-insert into photos values( 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, "testPhotoRetrieval1" );
-insert into photos values( 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, "testPhotoRetrieval2" );
-
+delete from collection_photos;
 insert into collection_photos values( 105, 2 );
 insert into collection_photos values( 105, 3 );
