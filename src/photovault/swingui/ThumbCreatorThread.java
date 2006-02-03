36b5f2d622d51cb194b930aacf9d560171c2e2fb
Harri Kaimio <harri@kaimio.fi>
2006-02-03 22:21:16 +0000

    Added Javadoc
    modified   src/photovault/swingui/ThumbCreatorThread.java
    Javadoc comments added

--- src/photovault/swingui/ThumbCreatorThread.java
+++ src/photovault/swingui/ThumbCreatorThread.java
@@ -4,11 +4,24 @@ import java.lang.*;
 import javax.swing.*;
 import imginfo.*;
 
+/**
+ ThumbCreatorThread is used for deferred creation of thumbnails by PhotoCollectionThumbView.
+ It starts a new thread that waits for thumbnail creation requests and notifies 
+ the PhotoCollectionThumbVew object when the requested thumbnail is ready.
+ <p>
+ This class implements a simple producer-consumer model for passing photos between the 2 
+ threads.
+ 
+ */
 
 class ThumbCreatorThread extends Thread {
     static org.apache.log4j.Logger log 
 	= org.apache.log4j.Logger.getLogger( ThumbCreatorThread.class.getName() );
 
+    /**
+     Constructor
+     @param view The view that will be notified after the thumbnail has been drawn
+     */
     public ThumbCreatorThread( PhotoCollectionThumbView view ) {
         super( "ThumbCreator" );
 	this.view = view;
@@ -17,16 +30,29 @@ class ThumbCreatorThread extends Thread {
     PhotoCollectionThumbView view;
     PhotoInfo photo;
     
+    /**
+     Submits a request for creation of a new thumbnail. This method returns immediately
+     after notifying the thumbnail creation thread. However, it blocks on a monitor
+     until the thread is free to take another job. So in performance critical situations
+     it is safest to call isBusy() before claling this.
+     @param photo the photo for which the thumbnail will be created.
+     */
     synchronized public void createThumbnail( PhotoInfo photo ) {
 	log.debug( "createThumbnail for " + photo.getUid() );
 	this.photo = photo;
 	notify();
     }
 
+    /**
+     @return true if the thread is currently working on a new thumbnail, false otherwise.
+     */
     public boolean isBusy() {
 	return ( photo != null );
     }
 
+    /**
+     Actual code for the thread.
+     */
     public void run() {
 	synchronized ( this ) {
 	    while ( true ) {
