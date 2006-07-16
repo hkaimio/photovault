8b5bd7766f0d5b11a9d3560eb3bffdc5c20b7b48
Harri Kaimio <harri@kaimio.fi>
2006-07-16 20:29:38 +0000

    Added separate export diaplog for setting export resolution.

--- src/main/java/org/photovault/swingui/ExportSelectedAction.java
+++ src/main/java/org/photovault/swingui/ExportSelectedAction.java
@@ -21,6 +21,8 @@
 package org.photovault.swingui;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -29,6 +31,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import org.photovault.imginfo.*;
 import org.photovault.imginfo.PhotoInfo;
+import org.photovault.swingui.export.ExportDlg;
 
 /**
    This action class implements exporting of all the selected images from a certain thumbnail
@@ -57,28 +60,98 @@ class ExportSelectedAction extends AbstractAction implements SelectionChangeList
     }
     
     public void actionPerformed( ActionEvent ev ) {
-	// Show the file chooser dialog
-	JFileChooser fc = new JFileChooser();
-	fc.addChoosableFileFilter( new ImageFilter() );
-	fc.setAccessory( new ImagePreview( fc ) );
-	
-	int retval = fc.showDialog( view, "Export image" );
-	if ( retval == JFileChooser.APPROVE_OPTION ) {
-       	    Container c = view.getTopLevelAncestor();
-	    Cursor oldCursor = c.getCursor();
+        File exportFile = null;
+        if ( view.getSelectedCount() > 1 ) {
+            exportFile = new File( "image_$n.jpg" );
+        } else {
+            exportFile = new File( "image.jpg" );
+        }
+        ExportDlg dlg = new ExportDlg( null, true );
+        dlg.setFilename( exportFile.getAbsolutePath() );
+
+        int retval = dlg.showDialog();        
+        if ( retval == ExportDlg.EXPORT_OPTION ) {
+            Container c = view.getTopLevelAncestor();
+            Cursor oldCursor = c.getCursor();
             c.setCursor( new Cursor( Cursor.WAIT_CURSOR ) );
-            File exportFile = fc.getSelectedFile();
-	    Collection selection = view.getSelection();
-	    if ( selection != null ) {
-		Iterator iter = selection.iterator();
-		if ( iter.hasNext() ) {
-		    PhotoInfo photo = (PhotoInfo) iter.next();
-		    photo.exportPhoto( exportFile, -1, -1 );
-		}
-	    }
+            String exportFileTmpl = dlg.getFilename();
+            int exportWidth = dlg.getImgWidth();
+            int exportHeight = dlg.getImgHeight();
+            Collection selection = view.getSelection();
+            if ( selection != null ) {
+                Iterator iter = selection.iterator();
+                if ( selection.size() > 1 ) {
+                    String format = getSequenceFnameFormat( exportFileTmpl );
+                    int n = 1;
+                    while ( iter.hasNext() ) {
+                        String fname = String.format( format, new Integer( n ) );
+                        File f = new File( fname );
+                        PhotoInfo photo = (PhotoInfo) iter.next();
+                        photo.exportPhoto( f, exportWidth, exportHeight );
+                        n++;
+                    }
+                } else {
+                    if ( iter.hasNext() ) {
+                        PhotoInfo photo = (PhotoInfo) iter.next();
+                        photo.exportPhoto( new File( exportFileTmpl ), 
+                                exportWidth, exportHeight );
+                    }
+                }
+            }
             c.setCursor( oldCursor );
-	}
+        }
     }
 
+    /**
+     Returns a proper filename in a numbered sequence. Examples:
+     <table>
+     <tr>
+     <td>pattern</td>
+     <td>example file names</td>
+     </tr>
+     <tr>
+     <td>photo.jpg</td>
+     <td>photo1.jpg, photo2.jpg</td>
+     </tr>
+     <tr>
+     <td>photo_$n.jpg</td>
+     <td>photo_1.jpg, photo_2.jpg, ..., photo_10000.jpg, ...</td>
+     </tr>
+     <tr>
+     <td>photo_$4n.jpg</td>
+     <td>photo_0001.jpg, photo_0002.jpg, ..., photo_10000.jpg, ...</td>
+     </tr>
+     </table>     
+     */
+    
+    String getSequenceFnameFormat( String seqBase ) {
+        seqBase = seqBase.replaceAll( "%", "%%" );
+        StringBuffer formatStrBuf = new StringBuffer( seqBase );
+        Pattern seqNumPattern = Pattern.compile( "\\$(\\d*)n");
+        Matcher m = seqNumPattern.matcher( seqBase );
+        if ( m.find() ) {
+            int start = m.start();
+            int end = m.end();
+            
+            //Check padding
+            String padStr = m.group( 1 );
+            if ( padStr.length() > 0 ) {
+                padStr = "0" + padStr;
+            }
+            String seqNumFormat = "%1$" + padStr + "d";
+            formatStrBuf.replace( start, end, seqNumFormat );
+        } else {
+            // No format template found, add number just before extension
+            Pattern extPattern = Pattern.compile( "\\.[^\\.]+$" );
+            int seqNumPos = seqBase.length();
+            Matcher extMatcher = extPattern.matcher( seqBase );
+            if ( extMatcher.find() ) {
+                seqNumPos = extMatcher.start();
+            } 
+            formatStrBuf.insert( seqNumPos, "%d" );
+        }
+        return formatStrBuf.toString();
+    }
+    
     PhotoCollectionThumbView view;
 }
