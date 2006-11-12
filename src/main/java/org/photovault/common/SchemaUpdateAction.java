c6e3328f32f7cdadf36d2cffebc309185294c655
Harri Kaimio <harri@kaimio.fi>
2006-11-12 19:15:34 +0000

    Merged revisions 574 via svnmerge from https://svn.sourceforge.net/svnroot/pvault/branches/raw-support
    ........
      r574 | hkaimio | 2006-11-12 21:07:21 +0200 (Sun, 12 Nov 2006) | 5 lines
    
      Fixed #63 in photovault-raw branch:
      Disabled XML validation in SchemaUpdateAction since currently
      photovault_schema.xml does not pass it for some reason.
    ........

--- src/main/java/org/photovault/common/SchemaUpdateAction.java
+++ src/main/java/org/photovault/common/SchemaUpdateAction.java
@@ -94,7 +94,9 @@ public class SchemaUpdateAction {
         
         // Get the database schema XML file
         InputStream schemaIS = getClass().getClassLoader().getResourceAsStream( "photovault_schema.xml" );
-        Database dbModel = new DatabaseIO().read( new InputStreamReader( schemaIS ) );
+        DatabaseIO dbio = new DatabaseIO();
+        dbio.setValidateXml( false );
+        Database dbModel = dbio.read( new InputStreamReader( schemaIS ) );
         
         // Alter tables to match corrent schema
         PersistenceBroker broker = PersistenceBrokerFactory.createPersistenceBroker( connKey );
